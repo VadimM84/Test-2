fed780123eebd29a849fc931c97fc310599ea930
Vadim <vmutovalov24@gmail.com>
2022-12-17 14:05:44 +0500

    add Example005_ConditionefElse

---
+++ Example005_ConditionefElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользовотеля: ");
+String username = Console.ReadLine();
+
+if(username.ToLower() == "нелечка")
+{
+   Console.WriteLine("Ура, это же Нелечка к нам пришла!"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
